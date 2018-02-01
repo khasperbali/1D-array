cceeb546343c18955f0f38ea024d0abc478b6593
Students <khasperbali@gmail.com>
2018-01-31 14:40:47 -1000

    work on this

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ function cars(){
 	var car3 = "Mazda";
 
 	//Create an array declaration named cars and set each car variable as the values.
-
+	var cars = ["Toyota", "Jeep Wrangler", "Mazda"];
 	//Print the created array
-
+	console.log(cars);
 	//Print the array length
-
+	console.log(cars.length)
 	//Return the created array and the array length
-	return;
-}
+	return (cars + "" + cars.length);
+};
 
 console.log(cars());
 
@@ -28,14 +28,14 @@ console.log(cars());
 */
 function order(food, drink, dessert){
 	//Store the parameters in an array called "order"
-
+	var order = [food, drink, dessert];
 	//Print the array named "order"
-	
+	console.log(order);
 	//Print the length of the array named "order"
-
+	console.log(order.length);
 	//Return the order as a complete sentence represented as a string
-	return;
-}
+	return ("I have ordered a " + order + ".");
+};
 
 console.log(order("Burger", "root beer", "frosty"));
 
@@ -57,11 +57,13 @@ var currentClasses = [
 */
 function loopToChange(classes){
 	//Print the parameter that is being passed through the function
-
+	console.log(classes)
 	//Use a For loop to iterate through each value in the passed array
-	
+	for (i = 0; i < currentClasses.length; i++){
+		console.log(currentClasses[i]);
+	}
 	//Return classes
-	return;
+	return ;
 }
 
 console.log(loopToChange(currentClasses));
@@ -74,9 +76,9 @@ console.log(loopToChange(currentClasses));
 */
 function arrayMethodToLoop(numbers){
 	//Print the parameter that is being passed through the function
-
+	console.log(numbers);
 	//Use the forEach() method to add 5 to each value in the passed array
-	
+	arrayMethodToLoop.forEach(numbers)
 	//Return the changed array
 	return;
 }
